763e90312b64560066c5b8a4de9a6bfbff811d63
Sam Dixon <sam@serenade.ai>
2021-04-14 17:07:29 -0700

    undo formatting change in mac.hpp

--- src/mac.hpp
+++ src/mac.hpp
@@ -11,41 +11,40 @@
 #define kAXEnhancedUserInterfaceAttribute CFSTR("AXEnhancedUserInterface")
 #define kAXManualAccessibilityAttribute CFSTR("AXManualAccessibility")
 
-namespace driver
-{
+namespace driver {
 
-    void Click(const std::string &button, int count);
-    bool ClickButton(AXUIElementRef element, const std::string &button, int count);
-    void ClickButton(const std::string &button, int count);
-    AXUIElementRef CreateActiveTextFieldRef();
-    AXUIElementRef CreateActiveWindowRef();
-    CFArrayRef CreateChildrenArray(AXUIElementRef element);
-    void Describe(AXUIElementRef element);
-    void FocusApplication(const std::string &application);
-    std::string GetActiveApplication();
-    int GetActivePid();
-    void GetClickableButtons(AXUIElementRef element,
-                             std::vector<std::string> &result);
-    std::vector<std::string> GetClickableButtons();
-    std::tuple<std::string, int> GetEditorState(bool fallback);
-    std::tuple<std::string, int> GetEditorStateFallback();
-    std::tuple<int, int> GetMouseLocation();
-    std::string GetRoleDescription(AXUIElementRef element);
-    std::string GetRawTitle(AXUIElementRef element);
-    std::vector<std::string> GetRunningApplications();
-    std::string GetTitle(AXUIElementRef element);
-    std::tuple<CGKeyCode, bool, bool> GetVirtualKeyAndModifiers(
-        const std::string &key);
-    bool HasActionName(AXUIElementRef element, CFStringRef name);
-    bool IsButton(AXUIElementRef element);
-    void MouseDown(const std::string &button);
-    void MouseUp(const std::string &button);
-    void PressKey(const std::string &key,
-                  const std::vector<std::string> &modifiers);
-    void SetEditorState(const std::string &source, int cursor, int cursorEnd);
-    void SetMouseLocation(int x, int y);
-    void ToggleKey(const std::string &key,
-                   const std::vector<std::string> &modifiers, bool down);
-    void ToLower(std::string &s);
+void Click(const std::string& button, int count);
+bool ClickButton(AXUIElementRef element, const std::string& button, int count);
+void ClickButton(const std::string& button, int count);
+AXUIElementRef CreateActiveTextFieldRef();
+AXUIElementRef CreateActiveWindowRef();
+CFArrayRef CreateChildrenArray(AXUIElementRef element);
+void Describe(AXUIElementRef element);
+void FocusApplication(const std::string& application);
+std::string GetActiveApplication();
+int GetActivePid();
+void GetClickableButtons(AXUIElementRef element,
+                         std::vector<std::string>& result);
+std::vector<std::string> GetClickableButtons();
+std::tuple<std::string, int> GetEditorState(bool fallback);
+std::tuple<std::string, int> GetEditorStateFallback();
+std::tuple<int, int> GetMouseLocation();
+std::string GetRoleDescription(AXUIElementRef element);
+std::string GetRawTitle(AXUIElementRef element);
+std::vector<std::string> GetRunningApplications();
+std::string GetTitle(AXUIElementRef element);
+std::tuple<CGKeyCode, bool, bool> GetVirtualKeyAndModifiers(
+    const std::string& key);
+bool HasActionName(AXUIElementRef element, CFStringRef name);
+bool IsButton(AXUIElementRef element);
+void MouseDown(const std::string& button);
+void MouseUp(const std::string& button);
+void PressKey(const std::string& key,
+              const std::vector<std::string>& modifiers);
+void SetEditorState(const std::string& source, int cursor, int cursorEnd);
+void SetMouseLocation(int x, int y);
+void ToggleKey(const std::string& key,
+               const std::vector<std::string>& modifiers, bool down);
+void ToLower(std::string& s);
 
-} // namespace driver
+}  // namespace driver
