75422e519b440d01ea8029a973dc7736c44b7101
Sam Dixon <sam@serenade.ai>
2021-05-17 13:16:17 -0700

    ensure windows is not null before releasing

--- src/mac.cpp
+++ src/mac.cpp
@@ -333,14 +333,16 @@ bool ActiveApplicationIsSandboxed() {
 int GetActivePid() {
   NSArray* windows = (NSArray*)CGWindowListCopyWindowInfo(
       kCGWindowListOptionOnScreenOnly | kCGWindowListExcludeDesktopElements, kCGNullWindowID);
-  for (NSDictionary* window in windows) {
-    int pid = [[window objectForKey:@"kCGWindowOwnerPID"] intValue];
-    if ([NSRunningApplication runningApplicationWithProcessIdentifier:pid].active) {
-      CFRelease(windows);
-      return pid;
+  if (windows != NULL) {
+    for (NSDictionary* window in windows) {
+      int pid = [[window objectForKey:@"kCGWindowOwnerPID"] intValue];
+      if ([NSRunningApplication runningApplicationWithProcessIdentifier:pid].active) {
+        CFRelease(windows);
+        return pid;
+      }
     }
+    CFRelease(windows);
   }
-  CFRelease(windows);
 
   NSRunningApplication* running = [NSWorkspace sharedWorkspace].frontmostApplication;
   if (running == NULL) {
