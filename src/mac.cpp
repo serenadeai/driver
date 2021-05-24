f7992b98bcfbf6191ec411ac2ed0cdd34c3e23e4
matt <matt@serenade.ai>
2021-05-24 10:02:30 -0700

    increase mac keystroke delay
    This was the lowest value I could pick without the keystroke test
    messing up on one of the sentences while in Slack.

--- src/mac.cpp
+++ src/mac.cpp
@@ -900,7 +900,7 @@ void ToggleKey(const std::string& key, const std::vector<std::string>& modifiers
   CFRelease(event);
 
   // determined empirically by typing into a variety of applications
-  usleep(4000);
+  usleep(8000);
 }
 
 void ToLower(std::string& s) {
